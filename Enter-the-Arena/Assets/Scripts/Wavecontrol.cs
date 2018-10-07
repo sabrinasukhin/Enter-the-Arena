9e161037b119ebcda0a80ee55c51d479c4bfc661
PaperbagLife <yunkunl@andrew.cmu.edu>
2018-10-07 15:01:42 -0400

    Test
    Test

---
+++ Enter-the-Arena/Assets/Scripts/Wavecontrol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class Wavecontrol : MonoBehaviour 
+{
+	public GameObject enemyNum;
+	public Vector3 spawnValues;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spawnWaves();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void spawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3 (Random.Range ())
+	}
+}
+*/
