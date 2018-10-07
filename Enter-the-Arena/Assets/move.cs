1bb4f6412ab25e65d3cfe124c011b0ff04d12482
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-10-07 16:52:54 -0400

    sphere creates a cube when it hits the ground

---
+++ Enter-the-Arena/Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//public GameObject testSphere;
+
+public class move : MonoBehaviour {
+   public GameObject enemy;
+    // Use this for initialization
+    void Start () {
+       // enemy = GameObject.Find("enemy");
+        transform.position = new Vector3(0f, 5f, 0f);
+        //test = Resources.Load("enemy") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(0.05f, 0f, 0f);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(enemy);
+        Destroy(gameObject);     
+    }
+}
