dea7a22f25b62f2dacf436057be313d7ef5bc6ac
PaperbagLife <yunkunl@andrew.cmu.edu>
2018-10-21 17:31:56 -0400

    This works properly

---
+++ Enter-the-Arena/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject[] enemies; //array of GameObeject to spawn.
+	public int[] enemyNum; //array of number of each enemy to spawn corresponding to enemy
+	public int enemyCount;
+	public float spawnWait;
+	public int enemyLeftInWave;
+	// Use this for initialization
+	void Start (){
+		StartCoroutine(spawnWave());
+	}
+	void spawn (GameObject enemy, int enemyNum){
+		for (int i = 0; i<enemyNum; i++){
+			enemy.transform.position = new Vector3(i*2, 10, 0);
+			Instantiate(enemy);
+			
+		}
+	}
+	IEnumerator spawnWave(){
+		for (int i = 0; i < enemies.Length; i++){
+			enemyLeftInWave = enemyCount;
+			spawn(enemies[i], enemyNum[i]);
+			yield return new WaitUntil(()=>enemyLeftInWave < 1);
+			yield return new WaitForSeconds(spawnWait);
+		}
+
+		//loop through enemy[]
+		//spawn enemyNum[] enemies
+		//wait until enemyleftinwave <1
+		enemyLeftInWave = enemyCount;
+		/*for (int i = 0; i<enemyCount; i++){
+			enemy1.transform.position = new Vector3(i*2, 10, 0);
+			Instantiate(enemy1);
+		}
+		yield return new WaitUntil(()=>enemyLeftInWave <1);
+		for (int i = 0; i<enemyCount; i++){
+			enemy2.transform.position = new Vector3((i*2)-4, 5, 0);
+			Instantiate(enemy2);
+		}*/
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Enter-the-Arena/Assets/Scripts/selfdestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfdestroy : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    	GameObject.Find("GameController").GetComponent<GameController>().enemyLeftInWave -=1;
+    	Destroy(gameObject);
+    }
+
+}
