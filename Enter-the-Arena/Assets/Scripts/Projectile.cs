db82caebafc8f273c53d9f2f840e403d72bc10d8
PaperbagLife <yunkunl@andrew.cmu.edu>
2018-10-28 16:15:31 -0400

    merged my version and bens
    Projectile added.

---
+++ Enter-the-Arena/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public Vector3 speed = new Vector3(1f,1f,0f);
+    public Vector3 gravity = new Vector3(0f,.05f,0f);
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        speed -= gravity;
+        gameObject.transform.position += speed;
+    }
+}
