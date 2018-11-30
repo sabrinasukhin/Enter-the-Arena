85aa7fac8717a21d44e3fa357d8a0ad668ed9de3
Sean Chang <seanchang9@gmail.com>
2018-11-29 19:00:02 -0500

    burst code

---
+++ Enter-the-Arena/Assets/Scripts/Burst.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burst : MonoBehaviour {
+
+	public GameObject projectile
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Probably don't have to handle that here
+		//Let Ricky deal with it LUL
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		if(collision.CompareTag("Offense")) {
+			//Explode
+			projectile.velocity = new Vector3(5, 1, 5);
+			Instantiate(projectile);
+			projectile.velocity = new Vector3(-5, 1, -5);
+			Instantiate(projectile);
+			projectile.velocity = new Vector3(-5, 1, 5);
+			Instantiate(projectile);
+			projectile.velocity = new Vector3(5, 1, -5);
+			Instantiate(projectile);
+		}
+	}
+}
